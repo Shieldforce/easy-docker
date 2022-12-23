1c51e15e1d9de9c3fa139da60d636e1c3c77cf41
Alexandre Ferreira <shieldforce2@gmail.com>
2022-12-23 02:30:12 -0300

    Dockerfile

--- src/Execute/Implements/ImplementPHP.php
+++ src/Execute/Implements/ImplementPHP.php
@@ -6,32 +6,81 @@ use App\Execute\Errors\StringError;
 
 class ImplementPHP
 {
-    public static function run($args)
-    {
-        self::version($args[2]);
-    }
+    private static string $version;
+    private static string $port;
+    private static string $container;
 
-    private static function version($version)
+    public static function run($argv)
     {
-        $stringCurrent = "--version=";
-        if(strpos($version, $stringCurrent) === false) {
-            $setColors = new SetColors;
-            $string = "Você precisa passar {$setColors->setEffect('blue')} ";
-            $string .= "{$stringCurrent}{version}{$setColors->setEffect('end')} ";
-            $string .= "{$setColors->setEffect('red')}como segundo parâmetro!
-             Se a implementação for PHP, aonde {version} é a versão escolhida! ";
-            $string .= "{$setColors->setEffect('end')}";
-            StringError::getError($string);
-            return;
+        $argsMount = [];
+        foreach ($argv as $arg) {
+            if($arg=="--php") {
+                continue;
+            }
+            $argMethod = false;
+            if(preg_match("/--(.*?)=/", $arg, $matches)) {
+                $argMethod = $matches[1] ?? false;
+                if($argMethod) self::$argMethod($arg);
+                if($argMethod) $argsMount[] = $arg;
+            }
+            if(!$argMethod) StringError::getErrorArg($argMethod);
         }
 
-        self::dockerRun(str_replace([$stringCurrent], [""], $version));
+        if(count($argsMount) > 0) self::dockerRun();
     }
 
-    private static function dockerRun(string $version)
+    private static function version($arg)
     {
         $path = str_replace(["/Execute/Implements"], [""], __DIR__);
-        exec($path."/dockers/php/{$version}/run.sh", $output);
+        $versionValue = str_replace(["--version="], [""], $arg);
+        if(!file_exists($path."/dockers/php/{$versionValue}/run.sh")) {
+            $setColors = new SetColors();
+            StringError::getError(
+                $setColors->setEffect("red").
+                "Versão {$versionValue} do PHP, ainda não foi implementada!".
+                $setColors->setEffect("end")
+            );
+        }
+        self::$version = $versionValue;
+    }
+
+    private static function port($arg)
+    {
+        $portValue = str_replace(["--port="], [""], $arg);
+        if(!is_numeric($portValue)) {
+            $setColors = new SetColors();
+            StringError::getError(
+                $setColors->setEffect("red").
+                "Porta {$portValue}, precisa ser um valor inteiro!".
+                $setColors->setEffect("end")
+            );
+        }
+        self::$port = $portValue;
+    }
+
+    private static function container($arg)
+    {
+        $containerValue = str_replace(["--container="], [""], $arg);
+        exec("docker ps --filter 'name={$containerValue}'", $output);
+        if(count($output) > 1) {
+            $message = "";
+            foreach ($output as $cont) {
+                $message .= $cont."\n";
+            }
+            StringError::getError(
+                "Existem containers com nome parecido : {$containerValue}! \n" .$message
+            );
+        }
+        self::$container = $containerValue;
+    }
+
+    private static function dockerRun()
+    {
+        $version    = self::$version;
+        $port       = self::$port;
+        $container  = self::$container;
+        $path       = str_replace(["/Execute/Implements"], [""], __DIR__);
+        exec($path."/dockers/php/{$version}/run.sh {$version} {$port} {$container}", $output);
         print_r($output);
     }
 }
