9ec9a9375da3f6032ada99c86edc5a72a4a4b5dc
Alexandre Ferreira <shieldforce2@gmail.com>
2022-12-22 22:56:56 -0300

    Versão php

--- src/start/run.sh
+++ src/start/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # shellcheck disable=SC1090
 
-path="\$(pwd)/vendor/shieldforce/easy-docker/src/scoob"
+path="/vendor/shieldforce/easy-docker/src/scoob"
 
 echo "" >> ~/.bashrc;
 echo "alias scoob='${path}'" >> ~/.bashrc;
