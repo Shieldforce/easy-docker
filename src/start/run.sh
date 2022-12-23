e02f631f457296297315fd53a4bae5aa50100603
Alexandre Ferreira <shieldforce2@gmail.com>
2022-12-22 22:41:42 -0300

    Versão php

--- src/start/run.sh
+++ src/start/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+# shellcheck disable=SC1090
 
 path="$(pwd)/vendor/shieldforce/easy-docker/src/scoob"
 
 echo "" >> ~/.bashrc;
 echo "alias scoob='php ${path}'" >> ~/.bashrc;
-source -/.bashrc
+source ~/.bashrc
 
 echo "Alias criado com sucesso!"
