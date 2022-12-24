246251ed25e3abfa82d17d37a879b5aec8b1e295
Alexandre Ferreira <shieldforce2@gmail.com>
2022-12-24 16:04:17 -0300

    Remount

--- src/dockers/php/8.1/run.sh
+++ src/dockers/php/8.1/run.sh
@@ -24,21 +24,22 @@ if [ -d $dir ]; then
   echo "Diretório Docker ok!"
 else
   mkdir $dir
-  cp ${scoob_dir}/docker/* >> $dir
-  chmod 777 ${dir}
 fi
 
-#docker build \
-#            -t ${container} \
-#            --build-arg PHP_VERSION=${version} \
-#            --build-arg EXPOSE_PORT=${port} \
-#            --build-arg SCOOB_DIR=${scoob_dir} \
-#            --build-arg ROOT_DIR=${root_dir} \
-#            -f "${scoob_dir}/Dockerfile" .
-#
-#docker run \
-#            -d \
-#            --name ${container} \
-#            -v "$(pwd):/var/www/" \
-#            -p "${port}:80" \
-#            ${container}
+cp -R ${scoob_dir}docker/* $dir
+chmod 777 $dir
+
+docker build \
+            -t ${container} \
+            --build-arg PHP_VERSION=${version} \
+            --build-arg EXPOSE_PORT=${port} \
+            --build-arg SCOOB_DIR=${scoob_dir} \
+            --build-arg ROOT_DIR=${root_dir} \
+            -f "${scoob_dir}/Dockerfile" .
+
+docker run \
+            -d \
+            --name ${container} \
+            -v "$(pwd):/var/www/" \
+            -p "${port}:80" \
+            ${container}
