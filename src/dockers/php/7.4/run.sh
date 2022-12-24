c1c513772ed56e9a96c6d1cfe1e33e0f92d763ce
Alexandre Ferreira <shieldforce2@gmail.com>
2022-12-24 11:07:18 -0300

    modify docker php

--- src/dockers/php/7.4/run.sh
+++ src/dockers/php/7.4/run.sh
@@ -3,9 +3,11 @@
 version=$1
 port=$2
 container=$3
+root_dir="vendor/shieldforce/easy-docker/src/dockers/php/${version}/"
 
 docker build \
             -t ${container} \
             --build-arg PHP_VERSION=${version} \
             --build-arg EXPOSE_PORT=${port} \
-            -f "vendor/shieldforce/easy-docker/src/dockers/php/${version}/Dockerfile" .
+            --build-arg ROOT_DIR=${root_dir} \
+            -f "${root_dir}/Dockerfile" .
