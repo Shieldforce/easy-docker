dbda9459f25c0e6222c4a4b67894e6d211b1f925
Alexandre Ferreira <shieldforce2@gmail.com>
2022-12-24 17:40:07 -0300

    Remount

--- src/dockers/laravel/run.sh
+++
@@ -1,7 +0,0 @@
-docker build \
-            -t ${CONTAINER} \
-            --build-arg REDIS_PORT=${REDIS_PORT} \
-            --build-arg MYSQL_PORT=${MYSQL_PORT} \
-            --build-arg WEB_PORT=${WEB_PORT} \
-            --build-arg PHP_PORT=${PHP_PORT} \
-            -f 'docker/runs/Dockerfile' .
